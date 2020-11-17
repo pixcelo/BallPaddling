b0eb6962c8c829b216b9cb43d06135273efca828
tetsu-upstr <bodomu@gmail.com>
2020-11-18 07:08:49 +0900

    add replay button

--- NewUnityProject/Assets/Scripts/BallScript.cs
+++ NewUnityProject/Assets/Scripts/BallScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallScript : MonoBehaviour
 {
@@ -8,13 +9,21 @@ public class BallScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = Random.Range(5f, 15f);
+        speed = Random.Range(5f, 10f);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(0f, 0f, 1 * speed * Time.deltaTime);
+
+        // ボールを逃したらゲームオーバー
+        if (transform.position.z > 15.0f)
+        {
+            //Debug.Log("GAVE OVER");
+            //Time.timeScale = 0; // ゲームを止める
+            SceneManager.LoadScene("GameOverScene");
+        }
     }
 
     // 何かと衝突したときの処理を記述
@@ -33,4 +42,6 @@ public class BallScript : MonoBehaviour
         }
         
     }
+
+
 }
